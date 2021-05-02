fb9444435a08060d9d8d30f5804be294553a91b5
Gumplen-kish <gumplen117@gmail.com>
2021-05-03 00:14:47 +0300

    Финищ

---
+++ source/js/faq.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+  var listFaq = document.querySelector('.faq__list');
+
+  if (listFaq) {
+    listFaq.classList.remove('faq__list--nojs');
+
+    var toggle = function (item) {
+      item.classList.toggle('faq__item--show');
+    };
+
+    listFaq.addEventListener('click', function (evt) {
+      var item = evt.target.closest('li');
+      toggle(item);
+    });
+  }
+})();
---
+++ source/js/login.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+  var esc = 27;
+  var openLogin = document.querySelector('.header-login');
+  var login = document.querySelector('.modal--login');
+  var closeLogin = document.querySelector('.login__close');
+  var inputEmail = document.querySelector('#login-email');
+
+  if (openLogin && login) {
+    openLogin.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      login.classList.add('modal--show');
+      document.body.style.overflow = 'hidden';
+      localStorage.setItem('email', inputEmail.value);
+      inputEmail.focus();
+    });
+  }
+
+  if (closeLogin) {
+    closeLogin.addEventListener('click', function () {
+      login.classList.remove('modal--show');
+      document.body.style.overflow = '';
+    });
+  }
+
+  function setKeyPressRemoveLoginHandler(evt) {
+    if (evt.keyCode === esc) {
+      login.classList.remove('modal--show');
+      document.body.style.overflow = '';
+    }
+  }
+
+  document.addEventListener('keydown', setKeyPressRemoveLoginHandler);
+
+  login.addEventListener('click', function (evt) {
+    var wrap = document.querySelector('.login__wrap');
+    if (!wrap.contains(evt.target)) {
+      login.classList.remove('modal--show');
+    }
+  });
+})();
