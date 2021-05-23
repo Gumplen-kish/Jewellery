b2896129baf9e9f8920a551c48fd91b3383dbda7
Gumplen-kish <gumplen117@gmail.com>
2021-05-24 00:34:44 +0300

    правка завершение

--- build/js/main.js
+++ build/js/main.js
@@ -162,6 +162,7 @@
 (function () {
   var header = document.querySelector('.header');
   var toggle = document.querySelector('.header__toggle');
+  var body = document.querySelector('body');
 
   if (header && toggle) {
     header.classList.remove('header--nojs');
@@ -170,9 +171,11 @@
       if (header.classList.contains('header--closed')) {
         header.classList.remove('header--closed');
         header.classList.add('header--opened');
+        body.classList.add('body_opened-header');
       } else {
         header.classList.add('header--closed');
         header.classList.remove('header--opened');
+        body.classList.remove('body_opened-header');
       }
     });
   }
